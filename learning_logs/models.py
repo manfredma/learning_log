5dd7331ef269b3fc06ac2636bb4f39be56443101
maxingfang <maxingfang@meituan.com>
2023-09-06 10:46:11 +0800

    修改字段名称: date_add -> date_added

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -15,7 +15,7 @@ class Entry(models.Model):
     """学到的有关某个主题的具体知识"""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
-    date_add = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name_plural = 'entries'
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -18,6 +18,6 @@ def topics(request):
 def topic(request, topic_id):
     """显示单个主题及其所有的条目."""
     topic = Topic.objects.get(id=topic_id)
-    entries = topic.entry_set.order_by('-date_add')
+    entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
